b5565dfc23185d70faadb8b9b16ead11e5c5349f
shaliniganesan <shalinig2309@gmail.com>
2024-07-17 20:01:44 +0530

    complete day1

---
+++ Day1/Variables&Datatype.js
@@ -0,0 +1,50 @@
+// Act1:variable declaration
+ 
+var num = 12;
+console.log("number:",num);
+
+let str = "shalini";
+console.log("string:",str);
+
+//Act2:constant declaration
+
+const bool = false;
+console.log("constant boolean:",bool);
+
+//Act3: Data types
+
+let num1 = 5;
+let str1 = "shaliniGanesan";
+let bool1 = true;
+let obj = {
+    name : "shalini",
+    city : "Thanjavur"
+};
+let arr = [1,2,3,4];
+
+console.log("num:",typeof(num1));
+console.log("str:",typeof(str1));
+console.log("bool:",typeof(bool1));
+console.log("obj:",typeof(obj));
+console.log("arr:",typeof(arr));
+
+//Act4: reassigning var
+
+let a = 25;
+console.log("before:",a);
+a = 50;
+console.log("after:",a);
+
+//Act5: reassigning const var
+const pi = 3.14;
+console.log("const before:",pi);
+pi = 5.7;
+console.log("const After:",pi);
+
+
+
+
+
+
+
+
