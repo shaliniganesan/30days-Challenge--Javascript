9778d6f8d55139554dc09b7d0bdf32c2a5a9e0aa
shaliniganesan <shalinig2309@gmail.com>
2024-07-18 06:04:49 +0530

    day2 completed

---
+++ Day2/Operators.js
@@ -0,0 +1,40 @@
+//Act1: Arithmetic operations
+
+let n1 = 5;
+let n2 = 3;
+
+console.log(n1+n2);
+console.log(n1-n2);
+console.log(n1*n2);
+console.log(n1/n2);
+console.log(n1%n2);
+
+//Act2: Assignment operators
+n1 += 6;
+console.log("op +=",n1);
+
+n1 -= 3;
+console.log("op -=",n1);
+
+//Act3: Comparison
+
+console.log(n1>n2);
+console.log(n1<n2);
+
+console.log(n1>=n2);
+console.log(n1<=n2);
+
+console.log(n1 == n2);
+console.log(n1 === n2);
+
+//Act4: logical operators
+
+console.log(n1 > n2 && n1 > 0);
+console.log(n1 > n2 || n2 > n1);
+console.log(!(n2>n1));
+
+//Act5 Ternary operator
+
+ console.log( n1 > 0 ? "positive" : "negative");
+
+ 
